89971ee16d593957f0766facff46cc4ceb4c2534
ignacioccugura <ignacioccugura@gmail.com>
2022-10-01 17:24:01 -0300

    avances

--- postInstall.sh
+++ postInstall.sh
@@ -6,4 +6,24 @@ function exaConfig(){
        	echo "alias l='exa -F --icons --color=always --group-direc>
        	echo "alias l.='exa -a | egrep "^\."'" >> ~/.zshrc
 }
+
+function vbInst(){
+	sudo dnf -y install @development-tools && sudo dnf -y install kernel-headers kernel-devel dkms elfutils-libelf-devel qt5-qtx11extras
+	cat <<EOF | sudo tee /etc/yum.repos.d/virtualbox.repo
+	[virtualbox]
+	name=Fedora $releasever - $basearch - VirtualBox
+	baseurl=http://download.virtualbox.org/virtualbox/rpm/fedora/36/\$basearch
+	enabled=1
+	gpgcheck=1
+	repo_gpgcheck=1
+	gpgkey=https://www.virtualbox.org/download/oracle_vbox.asc
+	EOF
+	sudo dnf search virtualbox
+	sudo dnf makecache --refresh
+	sudo dnf install VirtualBox-6.1
+	sudo usermod -a -G xboxusers $USER
+	newgrp xboxuser
+	id $USER
+}
 exaConfig
+vbInst
